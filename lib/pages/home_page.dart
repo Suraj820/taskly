931beaef2ad9abad65c485f342e7d6089583bc1a
suraj.pal <surajpal.enjay@gmail.com>
2023-07-05 09:31:27 +0530

    Added ListView and list tile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,60 @@
-
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   HomePage();
 
   @override
   State<StatefulWidget> createState() {
-   return _HomePageState();
+    return _HomePageState();
   }
-
-
-
 }
-class _HomePageState extends State<HomePage>{
+
+class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
   _HomePageState();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          "Taskly!",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+      body: _taskList(),
+      floatingActionButton: _addTaskButton(),
+    );
   }
 
+  Widget _taskList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "Do Workout",
+            style: TextStyle(decoration: TextDecoration.lineThrough),
+          ),
+          subtitle: Text(
+            DateTime.now().toString(),
+          ),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
 
-
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(Icons.add),
+      backgroundColor: Colors.red,
+    );
+  }
 }
